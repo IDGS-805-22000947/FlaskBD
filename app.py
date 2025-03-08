5237769eee492925263cf1a47766a4b17f6affbf
Damian Lopez <damislop04@gmail.com>
2025-03-08 13:13:13 -0600

    ABC Completo

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
      return render_template("index.html", form = create_form, alumnos = alumno)
 
 
+
 @app.route("/detalles", methods = ['GET', 'POST'])
 def detalles():
     create_from = forms.UserForm2(request.form)
@@ -38,11 +39,53 @@ def detalles():
         email = alum1.email
     return render_template("detalles.html", form = create_from , nombre = nombre, apaterno = apaterno, email = email)
 
+@app.route('/modificar', methods=['GET', 'POST'])
+def mdificar():
+    create_form= forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        # select * from alumnos where id == id
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=str.rstrip(alum1.nombre)
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.id = id
+        alum1.nombre=str.rstrip(create_form.nombre.data)
+        alum1.apaterno=create_form.apaterno.data
+        alum1.email=create_form.email.data
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('modificar.html', form=create_form)
+
+@app.route('/eliminar', methods=['GET','POST'])
+def eliminar():
+    create_form= forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        #select * from alumnos where id == id
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum = Alumnos.query.get(id)
+        #delete from alumnos where id=id
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('eliminar.html', form= create_form)
 
 @app.route('/Alumnos1', methods = ['GET', 'POST'])
 def Alumnos1():
     create_form = forms.UserForm2(request.form)
-    if request.method == 'POST':
+    if request.method== 'POST':
         alum=Alumnos(nombre=create_form.nombre.data,
                      apaterno = create_form.apaterno.data,
                      email = create_form.email.data)
